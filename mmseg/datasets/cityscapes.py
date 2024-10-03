44380248dce933b227f07d07a7ed657c8a117238
Konstantinos Tantoulas <kostastad@gmail.com>
2024-10-03 17:01:16 +0200

    data_root added

--- mmseg/datasets/cityscapes.py
+++ mmseg/datasets/cityscapes.py
@@ -24,7 +24,8 @@ class CityscapesDataset(BaseSegDataset):
 
     def __init__(self,
                  img_suffix='_leftImg8bit.png',
-                 seg_map_suffix='_gtFine_labelTrainIds.png',
+                 seg_map_suffix='_gtFine_labelTrainIds.png', 
+                 data_root = 'C:/Users/kosta/Documents/IPC/Quantization/mmrazor/data/cityscapes',
                  **kwargs) -> None:
         super().__init__(
-            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+            img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, data_root = data_root, **kwargs)
